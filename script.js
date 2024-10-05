05bfa906f446b58084175e43ba474df0a604407a
simokhin <nick.simokhin@yandex.ru>
2024-10-05 18:42:04 +0300

    Add function to put book on the shelf

--- script.js
+++ script.js
@@ -4,12 +4,28 @@ const myLibrary = [
         title: "Братья Карамазовы",
         pages: 900,
         read: true,
+        isRead: function(){
+            if (this.read){
+                return "Read";
+            }
+            else {
+                return "Not read";
+            };
+        }
     },
     {
         author: "Ф. М. Достоевский",
         title: "Идиот",
         pages: 640,
         read: true,
+        isRead: function(){
+            if (this.read){
+                return "Read";
+            }
+            else {
+                return "Not read";
+            };
+        }
     }
 ];
 
@@ -21,10 +37,27 @@ function addBookToLibrary(){
 
 };
 
-const list = document.querySelector(".books");
+const bookshelf = document.querySelector(".list");
 
 myLibrary.forEach(book => {
-    const li = document.createElement("li");
-    li.textContent = `${book.author}, ${book.title}, ${book.pages}, ${book.read}`;
-    list.appendChild(li);
+    const div = document.createElement("div");
+    div.classList.add("book")
+
+    const h3 = document.createElement("h3");
+    h3.textContent = `${book.author} - ${book.title}`;
+    div.appendChild(h3);
+
+    const p1 = document.createElement("p");
+    p1.textContent = `${book.pages} pages`;
+    div.appendChild(p1);
+
+    const p2 = document.createElement("p");
+    p2.textContent = `${book.isRead()}`;
+    div.appendChild(p2);
+
+    const button = document.createElement("button");
+    button.textContent = "x";
+    div.appendChild(button);
+
+    bookshelf.appendChild(div);
 })
